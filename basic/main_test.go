691ac4e2f807cb0cf10536aa86bbebbf8a3ead0f
Smuu <18609909+Smuu@users.noreply.github.com>
2023-05-23 14:11:51 +0200

    feat: add identifier
    Signed-off-by: Smuu <18609909+Smuu@users.noreply.github.com>

--- basic/main_test.go
+++ basic/main_test.go
@@ -1,17 +1,22 @@
 package basic
 
 import (
+	"fmt"
 	"github.com/celestiaorg/knuu/pkg/knuu"
 	"github.com/sirupsen/logrus"
 	"os"
 	"testing"
+	"time"
 )
 
 func TestMain(m *testing.M) {
-	err := knuu.Initialize()
+	t := time.Now()
+	identifier := fmt.Sprintf("%s_%03d", t.Format("20060102_150405"), t.Nanosecond()/1e6)
+	err := knuu.InitializeWithIdentifier(identifier)
 	if err != nil {
 		logrus.Fatalf("error initializing knuu: %v", err)
 	}
+	logrus.Infof("Unique identifier: %s", identifier)
 	exitVal := m.Run()
 	os.Exit(exitVal)
 }
