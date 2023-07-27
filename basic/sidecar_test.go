6f6caf26a76c1c1f3fc4128e07cf1a7e813b92be
Smuu <18609909+Smuu@users.noreply.github.com>
2023-07-27 11:01:43 +0200

    feat: add sidecar test
    Signed-off-by: Smuu <18609909+Smuu@users.noreply.github.com>

---
+++ basic/sidecar_test.go
@@ -0,0 +1,98 @@
+package basic
+
+import (
+	"github.com/celestiaorg/knuu/pkg/knuu"
+	"github.com/stretchr/testify/assert"
+	"os"
+	"testing"
+)
+
+func TestSidecar(t *testing.T) {
+	t.Parallel()
+	// Setup
+
+	executor, err := knuu.NewExecutor()
+	if err != nil {
+		t.Fatalf("Error creating executor: %v", err)
+	}
+
+	// web instance
+	web, err := knuu.NewInstance("web")
+	if err != nil {
+		t.Fatalf("Error creating instance '%v':", err)
+	}
+	if err := web.SetImage("docker.io/nginx:latest"); err != nil {
+		t.Fatalf("Error setting image '%v':", err)
+	}
+	if err := web.AddPortTCP(80); err != nil {
+		t.Fatalf("Error adding port: %v", err)
+	}
+	if _, err := web.ExecuteCommand("mkdir", "-p", "/usr/share/nginx/html"); err != nil {
+		t.Fatalf("Error executing command '%v':", err)
+	}
+	if err := web.AddFile("resources/html/index.html", "/usr/share/nginx/html/index.html", "0:0"); err != nil {
+		t.Fatalf("Error adding file '%v':", err)
+	}
+	if err := web.AddVolume("/usr/share/nginx/html", "1Gi"); err != nil {
+		t.Fatalf("Error adding volume '%v':", err)
+	}
+	if err := web.Commit(); err != nil {
+		t.Fatalf("Error committing instance: %v", err)
+	}
+
+	// sidecar instance
+	sidecar, err := knuu.NewInstance("sidecar")
+	if err != nil {
+		t.Fatalf("Error creating instance '%v':", err)
+	}
+	err = sidecar.SetImage("alpine:latest")
+	if err != nil {
+		t.Fatalf("Error setting image '%v':", err)
+	}
+	if err := sidecar.SetCommand("sleep", "infinity"); err != nil {
+		t.Fatalf("Error setting command '%v':", err)
+	}
+	if err := sidecar.Commit(); err != nil {
+		t.Fatalf("Error committing instance: %v", err)
+	}
+	if err := web.AddSidecar(sidecar); err != nil {
+		t.Fatalf("Error adding sidecar: %v", err)
+	}
+
+	t.Cleanup(func() {
+		// Cleanup
+		if os.Getenv("KNUU_SKIP_CLEANUP") == "true" {
+			t.Log("Skipping cleanup")
+			return
+		}
+
+		if err = executor.Destroy(); err != nil {
+			t.Fatalf("Error destroying executor: %v", err)
+		}
+
+		if err = web.Destroy(); err != nil {
+			t.Fatalf("Error destroying instance: %v", err)
+		}
+	})
+
+	// Test logic
+
+	webIP, err := web.GetIP()
+	if err != nil {
+		t.Fatalf("Error getting IP '%v':", err)
+	}
+
+	if err := web.Start(); err != nil {
+		t.Fatalf("Error starting instance: %v", err)
+	}
+	if err := web.WaitInstanceIsRunning(); err != nil {
+		t.Fatalf("Error waiting for instance to be running: %v", err)
+	}
+
+	wget, err := executor.ExecuteCommand("wget", "-q", "-O", "-", webIP)
+	if err != nil {
+		t.Fatalf("Error executing command '%v':", err)
+	}
+
+	assert.Equal(t, wget, "Hello World!\n")
+}
