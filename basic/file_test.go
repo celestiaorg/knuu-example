3cda31304e075db8b23173cef60ad388488c8e28
Mojtaba <mojtaba-esk@users.noreply.github.com>
2024-04-22 09:48:24 +0200

    feat: download file from running instance (#96)
    * feat: download file from running instance
    
    * fix: go mod remove clutter

--- basic/file_test.go
+++ basic/file_test.go
@@ -1,11 +1,13 @@
 package basic
 
 import (
+	"fmt"
 	"os"
 	"testing"
 
 	"github.com/celestiaorg/knuu/pkg/knuu"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestFile(t *testing.T) {
@@ -79,4 +81,41 @@ func TestFile(t *testing.T) {
 	}
 
 	assert.Equal(t, wget, "Hello World!\n")
+}
+
+func TestDownloadFileFromRunningInstance(t *testing.T) {
+	t.Parallel()
+	// Setup
+
+	target, err := knuu.NewInstance("target")
+	require.NoError(t, err, "Error creating instance")
+
+	require.NoError(t, target.SetImage("alpine:latest"), "Error setting image")
+	require.NoError(t, target.SetArgs("tail", "-f", "/dev/null"), "Error setting args") // Keep the container running
+	require.NoError(t, target.Commit(), "Error committing instance")
+	require.NoError(t, target.Start(), "Error starting instance")
+
+	t.Cleanup(func() {
+		// Cleanup
+		if os.Getenv("KNUU_SKIP_CLEANUP") == "true" {
+			t.Log("Skipping cleanup")
+			return
+		}
+
+		require.NoError(t, target.Destroy(), "Error destroying instance")
+	})
+
+	// Test logic
+
+	fileContent := "Hello World!"
+	filePath := "/hello.txt"
+
+	// Create a file in the target instance
+	out, err := target.ExecuteCommand("echo", "-n", fileContent, ">", filePath)
+	require.NoError(t, err, fmt.Sprintf("Error executing command: %v", out))
+
+	gotContent, err := target.GetFileBytes(filePath)
+	require.NoError(t, err, "Error getting file bytes")
+
+	assert.Equal(t, fileContent, string(gotContent))
 }
